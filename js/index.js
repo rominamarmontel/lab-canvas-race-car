0a376314a132b81afae5a44c8c27362b18d12187
romi <romi.varnier@gmail.com>
2023-02-05 22:23:29 +0100

    Iteration1 done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,18 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
 
-  function startGame() {}
+  const imgCar = new Image()
+  imgCar.src = "../images/car.png"
+  const imgRoad = new Image()
+  imgRoad.src = "../images/road.png"
+
+
+  function startGame() {
+    this.context = this.canvas.getContext("2d");
+  }
 };
